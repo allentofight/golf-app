5a4fa0518ba6ac0ae03c8c1fa0111838c4b73fb6
christoph.scheruebl@gmail.com <christoph.scheruebl@gmail.com@63a10333-ba03-0b32-6a5c-36ef440f9fe3>
2011-04-21 18:07:45 +0000

    first implementation of a data interface for golf courses (including demo data)

--- trunk/src/at/campus02/GolfApp/GolfApp.java
+++ trunk/src/at/campus02/GolfApp/GolfApp.java
@@ -1,13 +1,28 @@
 package at.campus02.GolfApp;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import at.campus02.GolfApp.data.GolfAppData;
 
 public class GolfApp extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// create DB entries
+		GolfAppData data = new GolfAppData(this);
+		data.insertGolfCourse(5, "Bad Gleichenberg", "Testbeschreibung");
+		data.insertGolfCourse(2, "Graz", "Testbeschreibung2");
+		data.insertGolfCourse(1, "Wien", "Testbeschreibung3");
+		data.insertGolfCourse(3, "Murau", "Testbeschreibung4");
+
+		Cursor cursor = data.allCourses(getParent());
+		while (cursor.moveToNext()) {
+			System.out.println(cursor.getInt(0) + " - " + cursor.getString(1)
+					+ " - " + cursor.getString(2));
+		}
+	}
 }
