aa7e57fe0751d0a4ef974b7bb7b17d15fe4e5517
willierhart@gmail.com <willierhart@gmail.com@63a10333-ba03-0b32-6a5c-36ef440f9fe3>
2011-04-22 10:05:17 +0000

    OnClickEvents and Layouts for Players, Courses, History, Settings

--- trunk/gen/at/campus02/GolfApp/R.java
+++ trunk/gen/at/campus02/GolfApp/R.java
@@ -18,26 +18,42 @@ public final class R {
         public static final int main=0x7f020001;
     }
     public static final class id {
-        public static final int continueRound=0x7f060003;
-        public static final int golfCourse=0x7f060005;
-        public static final int he1=0x7f060000;
-        public static final int history=0x7f060006;
-        public static final int newRound=0x7f060002;
-        public static final int player=0x7f060004;
-        public static final int settings=0x7f060007;
-        public static final int startImage=0x7f060001;
+        public static final int add=0x7f06000a;
+        public static final int cancel=0x7f06000b;
+        public static final int continueRound=0x7f060004;
+        public static final int golfCourse=0x7f060006;
+        public static final int he1=0x7f060001;
+        public static final int history=0x7f060007;
+        public static final int newRound=0x7f060003;
+        public static final int ok=0x7f060009;
+        public static final int player=0x7f060005;
+        public static final int settings=0x7f060008;
+        public static final int startImage=0x7f060002;
+        public static final int textView1=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int history=0x7f030000;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int manageplayer=0x7f030003;
+        public static final int selectgolfcourse=0x7f030004;
+        public static final int settings=0x7f030005;
     }
     public static final class string {
+        public static final int add=0x7f05000c;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f05000e;
         public static final int continueRound=0x7f050002;
+        public static final int delete=0x7f05000d;
         public static final int golfCourse=0x7f050004;
-        public static final int history=0x7f050005;
-        public static final int mainHeader=0x7f050007;
+        public static final int history=0x7f050006;
+        public static final int mainHeader=0x7f050009;
+        public static final int managePlayer=0x7f05000a;
         public static final int newRound=0x7f050001;
+        public static final int ok=0x7f05000b;
         public static final int player=0x7f050003;
-        public static final int settings=0x7f050006;
+        public static final int selectGolfCourse=0x7f050005;
+        public static final int settings=0x7f050008;
+        public static final int settingsShort=0x7f050007;
     }
 }
